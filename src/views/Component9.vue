138eba3a6bdec8055ac37b97469d3ff5aa7073b4
chenshuai <384619043@qq.com>
2018-11-06 21:22:57 +0800

    '多个datepicker联动的用法'

--- src/views/Component9.vue
+++ src/views/Component9.vue
@@ -3,15 +3,38 @@
         <h1>多个 DatePicker 联动的用法(联动禁用)</h1>
         <br><br>
 
-
-
-
+        <DatePicker v-model="startDate" @on-change="handleChange"></DatePicker>
+        <DatePicker v-model="endDate" :disabled="startDate === ''" :options="options" ref="endDate"></DatePicker>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Component9"
+        name: "Component9",
+        data () {
+            return {
+                startDate: '',
+                endDate: '',
+                options: {
+                    disabledDate: (date) => {
+                        return this.startDate >= date;
+                    }
+                }
+            }
+        },
+        methods: {
+            handleChange () {
+                if (this.startDate !== '') {
+                    this.$refs.endDate.handleFocus();
+                    if (this.endDate < this.startDate) {
+                        this.endDate = '';
+                    }
+                }else {
+                    this.endDate = '';
+                }
+            }
+
+        }
     }
 </script>
 
